6eebcc9d43324169513eae9ad36f54602c90433e
Lukas Lundin <lukasedv@outlook.com>
2021-07-07 00:19:44 +0300

    fixed json parsing

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 
-app.use(cors())
 app.use(express.static('build'))
+app.use(cors())
+app.use(express.json())
+
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :response-time ms - :body '))
